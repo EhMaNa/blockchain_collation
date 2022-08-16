52bfc5a43f109f21df5a5097daef9d92d65ad331
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-08-16 10:47:53 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const CollationContract = require('./lib/collation-contract');
+
+module.exports.CollationContract = CollationContract;
+module.exports.contracts = [ CollationContract ];
