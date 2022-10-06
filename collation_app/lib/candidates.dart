aec8ade71c8cfbc16b1fef64eb45888e2c8ff7dd
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-06 16:29:04 +0000

    initial commit

---
+++ collation_app/lib/candidates.dart
@@ -0,0 +1,10 @@
+class Candidate {
+  final int id;
+  final String name;
+  final String voteCount;
+  Candidate({
+    required this.id,
+    required this.name,
+    required this.voteCount,
+  });
+}
