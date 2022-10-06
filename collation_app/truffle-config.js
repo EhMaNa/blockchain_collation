5e46a68fdf06a244f476d891175dfdd81880b130
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-06 15:28:56 +0000

    made connection

--- collation_app/truffle-config.js
+++ collation_app/truffle-config.js
@@ -1,3 +1,79 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
+  contracts_directory: "./contracts",
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  db: {
+    enabled: false,
+  },
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -7,7 +83,7 @@
  * More information about configuration can be found at:
  *
  * https://trufflesuite.com/docs/truffle/reference/configuration
- * 
+ *
  * Hands-off deployment with Infura
  * --------------------------------
  *
@@ -15,29 +91,29 @@
  * Use this approach to make deployment a breeze 🏖️:
  *
  * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
- * to sign transactions before they're sent to a remote public node. 
+ * to sign transactions before they're sent to a remote public node.
  * Infura accounts are available for free at 🔍: https://infura.io/register
  *
  * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file. 
- * In your project root, run `$ npm install dotenv`. 
- * Create .env (which should be .gitignored) and declare your MNEMONIC 
+ * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * In your project root, run `$ npm install dotenv`.
+ * Create .env (which should be .gitignored) and declare your MNEMONIC
  * and Infura PROJECT_ID variables inside.
  * For example, your .env file will have the following structure:
- * 
+ *
  * MNEMONIC = <Your 12 phrase mnemonic>
  * PROJECT_ID = <Your Infura project id>
- * 
+ *
  * Deployment with Truffle Dashboard (Recommended for best security practice)
  * --------------------------------------------------------------------------
- * 
+ *
  * Are you concerned about security and minimizing rekt status 🤔?
  * Use this method for best security:
- * 
- * Truffle Dashboard lets you review transactions in detail, and leverages 
- * MetaMask for signing, so there's no need to copy-paste your mnemonic. 
- * More details can be found at 🔎: 
- * 
+ *
+ * Truffle Dashboard lets you review transactions in detail, and leverages
+ * MetaMask for signing, so there's no need to copy-paste your mnemonic.
+ * More details can be found at 🔎:
+ *
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
@@ -45,7 +121,7 @@
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-
+/*
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -56,7 +132,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
+/*
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -139,3 +215,5 @@ module.exports = {
   //   }
   // }
 };
+
+*/
