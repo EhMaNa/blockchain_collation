9c59224e792922dd03351d4ae615dfbc707e2ccc
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-12 12:30:30 +0000

    building collation display

--- collation_app/lib/show_collation.dart
+++ collation_app/lib/show_collation.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:collation_app/mini_database.dart';
 
@@ -11,7 +13,49 @@ class Show extends StatefulWidget {
 }
 
 class _ShowState extends State<Show> {
-  List collate = display(realColl);
+  @override
+  void initState() {
+    super.initState();
+    getCollation().then((value) {
+      setState(() {
+        if (value != null) value.forEach((item) => use.add(item));
+      });
+      //realColl = getCollation();
+      print(use.runtimeType);
+      print(use);
+    });
+  }
+
+  List use = [];
+  dynamic getCollation() async {
+    try {
+      var response = await Dio().get('http://localhost:3000/collation');
+      List list = response.data.toList();
+      //print(response.data.runtimeType);
+      //print(list.runtimeType);
+      //print(list);
+      List<Widget> wid = [];
+      for (int i = 0; i < list.length; i++) {
+        for (int j = 0; j < list[i]['candidates'].length; j++) {
+          //print(param[j]['candidates'].length);
+          //print(param[i]['candidates'][j]);
+          wid.add(ListTile(
+            title: Text(list[i]['candidates'][j]['name']),
+            trailing: Text(list[i]['candidates'][j]['voteCount']),
+          ));
+        }
+        wid.add(const Divider(thickness: 7.0));
+      }
+      //print(wid.runtimeType);
+      //print(wid);
+      return wid.toList();
+
+      //return list;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +63,10 @@ class _ShowState extends State<Show> {
         title: const Text('Collations'),
       ),
       body: ListView.builder(
-        itemCount: collate.length,
+        itemCount: use.length,
         itemBuilder: (context, index) {
           return Container(
-            child: collate[index],
+            child: use[index],
           );
           /*ListTile(
             title: Text(realColl[index]['candidates'][index]['name']),
@@ -34,7 +78,7 @@ class _ShowState extends State<Show> {
   }
 }
 
-List display(List param) {
+dynamic display(List param) {
   List<Widget> wid = [];
   for (int i = 0; i < param.length; i++) {
     for (int j = 0; j < param[i]['candidates'].length; j++) {
