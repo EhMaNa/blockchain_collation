e067521608fd48144c96d764f52e7ba570781c6d
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-14 14:50:31 +0000

    built UI for these screens

---
+++ collation_app/lib/screens/authorize.dart
@@ -0,0 +1,174 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:collation_app/global.dart';
+import 'package:flutter/material.dart';
+
+// Some form of Login Screen
+
+class Authorize extends StatelessWidget {
+  const Authorize({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordcontroller = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log In'),
+      ),
+      body: level == 'Officer'
+          ? Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                      textCapitalization: TextCapitalization.words,
+                      maxLength: 20,
+                      decoration: InputDecoration(
+                        hintText: 'Name',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                      ),
+                      controller: _emailController,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Please enter your name' : null,
+                      onChanged: (value) {
+                        String name = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    // ignore: sort_child_properties_last
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                      ),
+                      obscureText: true,
+                      controller: _passwordcontroller,
+                      validator: (value) => value!.length < 6
+                          ? 'Password should contain more than 6 Characters'
+                          : null,
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            primary: Colors.red,
+                            side: BorderSide(
+                              color: Colors.red,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        onPressed: () {
+                          /*if (_formKey.currentState!.validate()) {
+                           _validateAndSignUp();
+
+                          _formKey.currentState!.save();
+                        }*/
+                        },
+                        child: Container(
+                            width: 200,
+                            height: 50,
+                            child: Center(
+                                child: Text('Log In ',
+                                    style: TextStyle(
+                                        fontSize: 17.0,
+                                        fontWeight: FontWeight.w600))))),
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                      textCapitalization: TextCapitalization.words,
+                      maxLength: 20,
+                      decoration: InputDecoration(
+                        hintText: 'Key',
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red)),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                      ),
+                      controller: _emailController,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Please enter your name' : null,
+                      onChanged: (value) {
+                        String name = value;
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            primary: Colors.red,
+                            side: BorderSide(
+                              color: Colors.red,
+                            ),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                        onPressed: () {
+                          /*if (_formKey.currentState!.validate()) {
+                           _validateAndSignUp();
+
+                          _formKey.currentState!.save();
+                        }*/
+                        },
+                        child: Container(
+                            width: 200,
+                            height: 50,
+                            child: Center(
+                                child: Text('Log In ',
+                                    style: TextStyle(
+                                        fontSize: 17.0,
+                                        fontWeight: FontWeight.w600))))),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
---
+++ collation_app/lib/screens/level.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:collation_app/global.dart';
+import 'package:flutter/material.dart';
+
+// Choose which access level you are
+class Level extends StatelessWidget {
+  const Level({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Collation dApp'),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                level = 'Officer';
+                Navigator.pushNamed(context, '/login');
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 5,
+              ),
+              child: Text('Officer'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                level = 'Party';
+                Navigator.pushNamed(context, '/login');
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 5,
+              ),
+              child: Text('Party'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
