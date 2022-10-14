36bb4bbb191fe99864a6777cf7d9641dde848021
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-14 20:13:59 +0000

    allowed multiProvider

--- collation_app/lib/main.dart
+++ collation_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:collation_app/models/temp.dart';
+import 'package:collation_app/screens/add_candidate.dart';
 import 'package:collation_app/screens/authorize.dart';
 import 'package:flutter/material.dart';
 import 'package:collation_app/home_screen.dart';
@@ -7,11 +9,18 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
+      /*ChangeNotifierProvider(
       create: (context) => CandidateServices(),
       child: const MyApp(),
-    ),
-  );
+    ),*/
+      MultiProvider(
+    providers: [
+      ChangeNotifierProvider<CandidateServices>(
+          create: (context) => CandidateServices()),
+      ChangeNotifierProvider<Temporary>(create: (context) => Temporary()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -26,9 +35,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        "/": (context) => Level(),
-        "/login": (context) => Authorize(),
-        "/home": (context) => HomeScreen(),
+        "/": (context) => const Level(),
+        "/login": (context) => const Authorize(),
+        "/home": (context) => const HomeScreen(),
+        "/add": (context) => const AddCandidate(),
       },
     );
   }
