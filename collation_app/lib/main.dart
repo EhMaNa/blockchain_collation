29795af708894eb7afa24ee22b390070c47650be
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-17 14:08:13 +0000

    Update main.dart

--- collation_app/lib/main.dart
+++ collation_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:collation_app/models/temp.dart';
 import 'package:collation_app/screens/add_candidate.dart';
 import 'package:collation_app/screens/authorize.dart';
+import 'package:collation_app/show_collation.dart';
 import 'package:flutter/material.dart';
 import 'package:collation_app/home_screen.dart';
 import 'package:collation_app/screens/level.dart';
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
         "/login": (context) => const Authorize(),
         "/home": (context) => const HomeScreen(),
         "/add": (context) => const AddCandidate(),
+        "/show": (context) => const Show(),
       },
     );
   }
