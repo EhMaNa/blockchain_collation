79e1b42ef7a4e589f12686e6bff12f001e99044d
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-10-31 17:05:19 +0000

    user class as database

--- collation_app/lib/models/users.dart
+++ collation_app/lib/models/users.dart
@@ -1,1 +1,19 @@
-
+class User {
+  final int id;
+  final String area;
+  final String password;
+  final String level;
+  User(
+    this.id,
+    this.area,
+    this.password,
+    this.level,
+  );
+  List<User> users = [
+    User(1, 'Legon A', 'awesomeA', 'pOfficer'),
+    User(2, 'Legon B', 'awesomeM', 'pOfficer'),
+    User(3, 'Legon', 'awesome', 'cOfficer'),
+    User(4, 'Legon A', 'awesomeR', 'Party'),
+    User(5, 'Legon B', 'awesomeE', 'Party'),
+  ];
+}
