0b8dddfd99d7c4bc6e32de4bd624161ea8b83e31
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-11-04 15:01:30 +0000

    moved file to a new directory

---
+++ collation_app/lib/custom/global.dart
@@ -0,0 +1,16 @@
+//Keep global variables
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:collation_app/models/users.dart';
+
+String level = '';
+String ID = '';
+String ID1 = 'Legon A';
+String ID2 = 'Legon B';
+String ID3 = 'Legon';
+String password1 = 'awesomeR';
+String password2 = 'awesomeE';
+String password3 = 'awesomeA';
+String password4 = 'awesomeM';
+String password5 = 'Officer';
+User currentUser = User(1234, 'admin', 'admin', 'admin');
