fa2695ded753b6ce973e4fab308d65411f55058d
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-12-03 13:51:18 +0000

    refactored file

--- collation_app/lib/custom/global.dart
+++ collation_app/lib/custom/global.dart
@@ -3,16 +3,7 @@
 //import 'package:dio/dio.dart';
 import 'package:collation_app/models/users.dart';
 
-String level = '';
-String ID = '';
-String ID1 = 'Legon A';
-String ID2 = 'Legon B';
-String ID3 = 'Legon';
-String password1 = 'awesomeR';
-String password2 = 'awesomeE';
-String password3 = 'awesomeA';
-String password4 = 'awesomeM';
-String password5 = 'Officer';
-User currentUser = User(1234, 'admin', 'admin', 'admin');
+User currentUser = User(1234, 'admin', 'admin', 'admin', 0);
+List<User> logs = [];
 String count = '';
-late List<dynamic> colate = [];
+List<dynamic> colate = [];
