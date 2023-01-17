fdd8b148606abe68831eba9913921c0cd12e4ac2
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-01-17 21:11:28 +0000

    refactor: default dart formatter

--- collation_app/lib/models/users.dart
+++ collation_app/lib/models/users.dart
@@ -4,10 +4,5 @@ class User {
   String password;
   String level;
 
-  User(
-    this.id,
-    this.area,
-    this.password,
-    this.level,
-  );
+  User(this.id, this.area, this.password, this.level);
 }
