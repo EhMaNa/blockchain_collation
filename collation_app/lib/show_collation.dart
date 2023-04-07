0c73af66b99dd76c3f0a2ea0eaaba8887d6528d8
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2023-04-07 14:17:50 +0000

    update: apply capitalize extension

--- collation_app/lib/show_collation.dart
+++ collation_app/lib/show_collation.dart
@@ -41,9 +41,3 @@ class _ShowState extends State<Show> {
     );
   }
 }
-
-extension StringExtension on String {
-  String capitalizeFirst() {
-    return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
-  }
-}
